8a2f369f3c3efc022e78b693415379822ca10489
Malte Linke <me@parzival.space>
2025-12-12 15:24:37 +0100

    Initialize Edge Cluster (#1)

---
+++ edge/nix/nodes/g11-server1/configuration.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../common.nix
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "g11-server1"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.enableIPv6 = true;
+
+  # configure firewall
+  # 6443 - Kubernetes API server
+  # 80 - HTTP
+  # 443 - HTTPS
+  # 8472 - Flannel VXLAN networking
+  networking.firewall.allowedTCPPorts = [ 6443 80 443 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
+
+  # K3s server configuration
+  sops.secrets."k3s/token" = { };
+  services.k3s = {
+    enable = true;
+    role = "server";
+    clusterInit = true;
+    tokenFile = config.sops.secrets."k3s/token".path;
+  };
+
+  # configure openscsi
+  environment.systemPackages = [ pkgs.nfs-utils ];
+  services.openiscsi = {
+    enable = true;
+    name = "${config.networking.hostName}-initiatorhost";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
