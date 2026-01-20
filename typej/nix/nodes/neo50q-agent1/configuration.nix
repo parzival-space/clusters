e3bc8bab20d557f00d3b7d9e32bd4a8ff2814ad9
Malte Linke <me@parzival.space>
2026-01-20 09:58:21 +0100

    Clean up comments in configuration.nix for clarity and conciseness

--- typej/nix/nodes/neo50q-agent1/configuration.nix
+++ typej/nix/nodes/neo50q-agent1/configuration.nix
@@ -41,12 +41,7 @@
     name = "${config.networking.hostName}-initiatorhost";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
